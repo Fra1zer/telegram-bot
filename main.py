8a9f436787855a41bb4295ec601e4e5d08655c7f
Artem <sidor.sidorovich.87@mail.ru>
2024-01-02 19:32:19 +0300

    new bot

--- main.py
+++ main.py
@@ -1,75 +1,106 @@
+import json
+import os
+
 import telebot
+from info import characters, questions, welcome_message
 from telebot import types
-import info
 
-token = '6379306935:AAE0TYPvoQysIQIQginClDrACOZWbSTFGJ0'
-
-bot = telebot.TeleBot(token=token)
+TOKEN = '6379306935:AAE0TYPvoQysIQIQginClDrACOZWbSTFGJ0'
+bot = telebot.TeleBot(TOKEN)
 
 
-def hello_filter(message):
-    hello = 'привет'
-    return hello in message.text.lower()
+def load_user_data():
+    with open("user_data.json", "r") as file:
+        return json.load(file)
 
 
-def bye_filter(message):
-    bye = 'пока'
-    return bye in message.text.lower()
+def save_user_data(user_data):
+    with open("user_data.json", "w") as file:
+        json.dump(user_data, file)
 
 
-@bot.message_handler(content_types=['text'], func=hello_filter)
-def say_hello(message):
-    bot.send_message(message.chat.id, f"И тебе привет, {message.from_user.first_name}!")
+if os.path.exists("user_data.json"):
+    user_data = load_user_data()
+else:
+    user_data = {}
 
 
-@bot.message_handler(content_types=['text'], func=bye_filter)
-def say_bye(message):
-    bot.send_message(message.chat.id, f"Пока, {message.from_user.first_name}!")
+@bot.message_handler(commands=["start"])
+def start_survey(message):
+    user_data[message.chat.id] = {character: 0 for character in characters}
+    user_data[message.chat.id]["question_index"] = 0
+
+    bot.send_message(message.chat.id, welcome_message)
+    send_question(message.chat.id)
+
+    save_user_data(user_data)
 
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, задохлик, напиши команду /help, чтобы узнать, что я могу')
+def send_question(chat_id):
+    question_index = user_data[chat_id]["question_index"]
+
+    question, options = questions[question_index]
+
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    for text, _ in options:
+        markup.add(text)
+    bot.send_message(chat_id, question, reply_markup=markup)
 
 
-@bot.message_handler(commands=['info'])
-def start_message(message):
-    bot.send_message(message.chat.id, info.description)
+@bot.message_handler(func=lambda message: True)
+def handle_answer(message):
+    if message.chat.id not in user_data:
+        bot.send_message(
+            message.chat.id,
+            "Пожалуйста, начните анкету с помощью команды /start",
+        )
+        return
+
+    chat_id = message.chat.id
+    question_index = user_data[chat_id]["question_index"]
+    _, options = questions[question_index]
+
+    selected_option = None
+    for option in options:
+        if option[0] == message.text:
+            selected_option = option[1]
+            break
+    if not selected_option:
+        bot.send_message(
+            chat_id, "Пожалуйста, выберите один из предложенных вариантов."
+        )
+        return
+
+    for character, score in selected_option.items():
+        user_data[chat_id][character] += score
+
+    user_data[chat_id]["question_index"] += 1
+    if user_data[chat_id]["question_index"] < len(questions):
+        send_question(chat_id)
+    else:
+        send_result(chat_id)
+
+    save_user_data(user_data)
 
 
-@bot.message_handler(commands=['contacts'])
-def start_message(message):
-    bot.send_message(message.chat.id, info.contacts)
+def send_result(chat_id):
+    scores = {
+        character: score
+        for character, score in user_data[chat_id].items()
+        if character != "question_index"
+    }
+
+    max_character = max(scores, key=scores.get)
+
+    description, image_name = characters[max_character]
+    caption_message = f"Ваш тип мышления: {max_character}\n{description}"
+
+    with open(image_name, "rb") as image:
+        bot.send_photo(chat_id, image, caption=caption_message)
+
+    del user_data[chat_id]
+
+    save_user_data(user_data)
 
 
-@bot.message_handler(commands=['projects'])
-def start_message(message):
-    bot.send_message(message.chat.id, info.projects)
-
-
-@bot.message_handler(content_types=["photo"])
-def get_photo(message):
-    mark = types.InlineKeyboardMarkup()
-    bot.reply_to(message, 'Молодец, хорошая картинка', reply_markup=mark)
-
-
-@bot.message_handler(commands=['help'])
-def information_message(message):
-    bot.send_message(message.chat.id, 'Вот список того, что я умею:\n'
-                                      '1.По команде /start готов к работе\n'
-                                      '2.По команде /help вывожу эту справку\n'
-                                      '3.Отвечаю на сообщение "привет"\n'
-                                      '4.Отвечаю на сообщение "пока"\n'
-                                      '5.Все другие сообщения дублирую\n'
-                                      '6.По команде /info вывожу информацию о создателе\n'
-                                      '7.По команде /contacts вывожу контакты создателя\n'
-                                      '8.По команде /projects вывожу контакты создателя\n'
-                                      '9.Отвечаю на картинки')
-
-
-@bot.message_handler(content_types=['text'])
-def repeat_message(message):
-    bot.send_message(message.chat.id, message.text)
-
-
-bot.polling()
+bot.polling(non_stop=True)
